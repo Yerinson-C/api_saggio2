5934169932becc73ef636c57e84068301e974f97
User <user@iMac-de-Admin.local>
2026-04-10 14:24:04 -0500

    Api conectada mysqli

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./preguntas.db");
 
-// Crear tabla
+// Crear tabla. --
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS preguntas (
